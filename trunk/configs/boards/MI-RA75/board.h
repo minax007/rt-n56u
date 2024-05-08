43bd43fa0d17e9fcf85abe5060258ab75a3c3c44
minax007 <67478561+minax007@users.noreply.github.com>
2024-05-08 11:59:03 +0200

    Create board.h

---
+++ trunk/configs/boards/MI-RA75/board.h
@@ -0,0 +1,26 @@
+/* Xiaomi Mi Range Extender AC1200 (RA75)*/
+
+#define BOARD_PID		"MI-RA75"
+#define BOARD_NAME		"MI-RA75"
+#define BOARD_DESC		"Xiaomi Mi Range Extender AC1200"
+#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"https://www.mi.com/"
+#define BOARD_MODEL_URL		"https://www.mi.com/global/product/mi-wifi-range-extender-ac1200"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	38
+#define BOARD_GPIO_BTN_WPS	7
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI 	11
+#define BOARD_GPIO_LED_POWER 	24 	/* 24: blue, 26: yellow, 29: red */
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	1
+#define BOARD_NUM_ANT_5G_RX	1
+#define BOARD_NUM_ANT_2G_TX	1
+#define BOARD_NUM_ANT_2G_RX	1
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
